56ffa8ff17ff0f5da637a56a0f49c946b9ae66d2
Lucas Ruggiero <lucas9005@gmail.com>
2015-12-01 04:34:48 -0300

    Implementados los Niveles de Zoom con sus Correspondientes Marcadores. Implementado Pronostico Extendido. Correcciones y Mejoras Varias.

--- api/obtenerCiudades.php
+++ api/obtenerCiudades.php
@@ -1,6 +1,6 @@
 <?php
 	// API CROTA PARA OBTENER LAS CIUDADES DEL PAIS INDICADO
-	if($_REQUEST['pais']){
+	if(isset($_REQUEST['pais'])){
 		$ciudades = file_get_contents('../config/ciudades/' . $_REQUEST['pais'] . '.ini');
 		if($ciudades){
 			echo $ciudades;
--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 // Clase Mapa
 function Mapa(){
 	this.opcionesMapa = {
-		zoom: 5,
-		center: new google.maps.LatLng( -40.300000, -65.136344),
-		scrollwheel: false,
+		zoom: 4,
+		center: new google.maps.LatLng( -40.000000, -65.000000),
 		mapTypeId: google.maps.MapTypeId.HYBRID
 	};
-	this.mapa = new google.maps.Map(document.getElementById('mapa'), this.opcionesMapa);
+	mapa = new google.maps.Map(document.getElementById('mapa'), this.opcionesMapa);
 	this.paises = [];
 	this.inicializar();
+	this.actualizarMarcadores();
 };
 
 // Metodo inicializar: Lee el Archivo de Configuracion de los Paises y los Crea
@@ -19,11 +19,33 @@ Mapa.prototype.inicializar = function(){
 		if(auxiliarPaises.readyState == 4){
 			var auxiliarLinea = auxiliarPaises.responseText.split(';');
 			for(var i = 0; i < auxiliarLinea.length; i++){
-				this.paises.push(new Pais($.trim(auxiliarLinea[i])));
+				if(typeof this.paises[auxiliarLinea[i]] == 'undefined'){
+					this.paises[auxiliarLinea[i]] = [];
+				};
+				this.paises[auxiliarLinea[i]] = new Pais($.trim(auxiliarLinea[i]));
 			};
 		};
 	}, this);
 	auxiliarPaises.send();
+};
+
+// Metodo actualizarMarcadores: Muestra u Oculta los Marcadores en Base al Zoom
+Mapa.prototype.actualizarMarcadores = function(){
+	for(var clavePais in this.paises){
+		for(var claveProvincia in this.paises[clavePais].provincias){
+			for(var claveCiudad in this.paises[clavePais].provincias[claveProvincia]){
+				if(claveCiudad != 0){
+					if(mapa.getZoom() == 7){
+						this.paises[clavePais].provincias[claveProvincia][claveCiudad].marcador.setMap(mapa);
+					} else if(mapa.getZoom() == 4){
+						this.paises[clavePais].provincias[claveProvincia][claveCiudad].marcador.setMap(null);
+					};
+				} else {
+					this.paises[clavePais].provincias[claveProvincia][claveCiudad].marcador.setMap(mapa);
+				};
+			};
+		};
+	};
 };
 
 // Clase Pais
@@ -58,13 +80,120 @@ Pais.prototype.inicializar = function(){
 };
 
 // Clase Ciudad
-function Ciudad(nombre, provincia, latitud, longitud){
-	this.nombre = nombre;
+function Ciudad(provincia, nombre, latitud, longitud){
 	this.provincia = provincia;
+	this.nombre = nombre;
 	this.latitud = latitud;
 	this.longitud = longitud;
-	this.temperatura = '';
-	this.iconoClima = '';
+	this.ventanaPronosticoExtendido = '';
+	this.marcador = '';
+	this.pronosticoExtendido = '<img src="imagenes/loading.png" class="center-block">';
+	this.temperatura = 'N/A';
+	this.iconoClima = 'imagenes/loading.png';
+	this.crearVentanaPronosticoExtendido();
+	this.crearMarcador();
+	this.obtenerClima();
 };
 
-var mapa = new Mapa();
+// Metodo obtenerClima: Hace una Consulta al Web Service del Clima
+Ciudad.prototype.obtenerClima = function(){
+	$.proxy(
+		$.ajax({
+		type: 'GET',
+		dataType: 'JSON',
+		url: 'http://api.openweathermap.org/data/2.5/find?lat=' + this.latitud + '&lon=' + this.longitud + '&cnt=1&units=metric&appid=2de143494c0b295cca9337e1e96b00e0',
+		success: $.proxy(
+					function(data){
+						this.temperatura = Math.floor(data.list[0].main.temp);
+						this.iconoClima = 'imagenes/' + data.list[0].weather[0].icon + '.png';
+						this.marcador.setTitle(this.provincia + ', ' + this.nombre + ' ' + this.temperatura + '*C');
+						this.marcador.setIcon(this.iconoClima);
+					}, this),
+		error: function(){
+					alert('Error al Obtener el Clima');
+				}
+	}), this);
+};
+
+// Metodo obtenerPronosticoExtendido: Hace una Consulta al Web Service del Clima
+Ciudad.prototype.obtenerPronosticoExtendido = function(){
+	$.proxy(
+		$.ajax({
+		type: 'GET',
+		async: true,
+		dataType: 'JSON',
+		url: 'http://api.openweathermap.org/data/2.5/find?lat=' + this.latitud + '&lon=' + this.longitud + '&cnt=5&units=metric&appid=2de143494c0b295cca9337e1e96b00e0',
+		success: $.proxy(
+					function(data){
+						$.proxy(
+							$.ajax({
+								url: 'js/templates/pronosticoExtendido.mst',
+								success: $.proxy(
+											function(template){
+												var variables = {
+													'nombre': this.nombre,
+													'tempMaxHoy': Math.floor(data.list[0].main.temp_max),
+													'tempMinHoy': Math.floor(data.list[0].main.temp_min),
+													'humedadHoy': Math.floor(data.list[0].main.humidity),
+													'presionHoy': Math.floor(data.list[0].main.pressure),
+													'tempMaxA': Math.floor(data.list[1].main.temp_max),
+													'tempMinA': Math.floor(data.list[1].main.temp_min),
+													'humedadA': Math.floor(data.list[1].main.humidity),
+													'presionA': Math.floor(data.list[1].main.pressure),
+													'tempMaxB': Math.floor(data.list[2].main.temp_max),
+													'tempMinB': Math.floor(data.list[2].main.temp_min),
+													'humedadB': Math.floor(data.list[2].main.humidity),
+													'presionB': Math.floor(data.list[2].main.pressure),
+													'tempMaxC': Math.floor(data.list[3].main.temp_max),
+													'tempMinC': Math.floor(data.list[3].main.temp_min),
+													'humedadC': Math.floor(data.list[3].main.humidity),
+													'presionC': Math.floor(data.list[3].main.pressure),
+													'tempMaxD': Math.floor(data.list[4].main.temp_max),
+													'tempMinD': Math.floor(data.list[4].main.temp_min),
+													'humedadD': Math.floor(data.list[4].main.humidity),
+													'presionD': Math.floor(data.list[4].main.pressure)
+												};
+												//console.log(data);
+												this.pronosticoExtendido = Mustache.render(template, variables);
+												this.ventanaPronosticoExtendido.setContent(this.pronosticoExtendido);
+										 }, this)
+						}), this);
+					}, this)
+	}), this);
+};
+
+// Metodo crearMarcador: Crea un Marcador con los Datos Propios
+Ciudad.prototype.crearMarcador = function(){
+	this.marcador = new google.maps.Marker({
+		position: new google.maps.LatLng(this.latitud, this.longitud),
+		title: this.provincia + ', ' + this.nombre + ' ' + this.temperatura + '*C',
+		icon: this.iconoClima,
+	});
+	this.marcador.addListener('click', $.proxy(function(){
+		this.obtenerPronosticoExtendido();
+		this.ventanaPronosticoExtendido.open(mapa, this.marcador);
+	}, this));
+};
+
+// Metodo crearVentanaEmergente: Crea una Ventana Emergente para Visualizar el Pronostico Extendido
+Ciudad.prototype.crearVentanaPronosticoExtendido = function(){
+	this.ventanaPronosticoExtendido = new google.maps.InfoWindow({
+		content: this.pronosticoExtendido
+	});
+};
+
+mapamundi = new Mapa();
+
+mapa.addListener('zoom_changed', function(){
+	if(mapa.getZoom() > 7){
+		mapa.setZoom(7);
+	} else if(mapa.getZoom() == 6){
+		mapa.setZoom(4);
+		mapamundi.actualizarMarcadores();
+	} else if(mapa.getZoom() == 5){
+		mapa.setZoom(7);
+		mapamundi.actualizarMarcadores();
+	} else if(mapa.getZoom() < 4){
+		mapa.setZoom(4);
+	};
+});
